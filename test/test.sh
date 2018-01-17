52d7a084b8ce15d49cb6566e57572dd77b73f18e
Jakob Kogler <jakob.kogler@gmail.com>
2018-01-17 14:25:56 +0100

    Use CXX as compiler

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for cppfile in *.cpp;
 do
-    g++ -std=c++14 $cppfile -o $cppfile.out && ./$cppfile.out
+    $CXX -std=c++14 $cppfile -o $cppfile.out && ./$cppfile.out
     rm $cppfile $cppfile.out
 done
